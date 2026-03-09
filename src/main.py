11c56d38190cdc4ea3074732fd82db3d08485c1b
JAV-7 <fjavier.ramosj@gmail.com>
2026-03-09 08:32:33 -0600

    feat: add ValueError in loop_until_correct

--- src/main.py
+++ src/main.py
@@ -11,7 +11,11 @@ def loop_until_correct(secret_number: int):
     comparison_result = None
     attempts = 0
     while True:
-        user_guess = int(input("Ingresa tu intento: "))
+        try:
+            user_guess = int(input("Ingresa tu intento: "))
+        except ValueError:
+            print("Por favor, ingresa un número válido.")
+            continue
         attempts += 1
         comparison_result = check_equality(user_guess, secret_number)
         if comparison_result == 0:
@@ -25,7 +29,7 @@ def loop_until_correct(secret_number: int):
 
 def start_game():
     secret_number = random.randint(1, 20)
-    print("Adivina el número entre 1 y 20")
+    print("Adivina el número entre 1 y 20.")
     attempts = loop_until_correct(secret_number)
     print("Número de intentos:", attempts)
 
