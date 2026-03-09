973222d52dd121b9819c246b4370c86214f6e601
JAV-7 <fjavier.ramosj@gmail.com>
2026-03-09 07:50:20 -0600

    style: rename variables

--- src/main.py
+++ src/main.py
@@ -1,18 +1,18 @@
 import random
 
 def start_game():
-    number = random.randint(1, 20)
-    guess = 0
+    secret_number = random.randint(1, 20)
+    user_guess = 0
     attempts = 0
     print("Adivina el número entre 1 y 20")
-    while guess != number:
-        guess = int(input("Ingresa tu intento: "))
+    while user_guess != secret_number:
+        user_guess = int(input("Ingresa tu intento: "))
         attempts += 1
-        if guess < number:
+        if user_guess < secret_number:
             print("Muy bajo")
-        elif guess > number:
+        elif user_guess > secret_number:
             print("Muy alto")
-        elif guess == number:
+        elif user_guess == secret_number:
             print("¡Correcto!")
         else:
             print("Error")
