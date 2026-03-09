ac7812c86e31f51c74c8e01274f3c08cca1275c6
JAV-7 <fjavier.ramosj@gmail.com>
2026-03-09 11:10:23 -0600

    feat: add valid and overall attempts

--- src/main.py
+++ src/main.py
@@ -7,16 +7,17 @@ def check_equality(user_guess: int, secret_number: int) -> int:
         return 1
     return -1
 
-def loop_until_correct(secret_number: int):
+def loop_until_correct(secret_number: int) -> list:
     comparison_result = None
-    attempts = 0
+    attempts = [0,0]
     while True:
         try:
             user_guess = int(input("Ingresa tu intento: "))
+            attempts[0] += 1
         except ValueError:
             print("Por favor, ingresa un número válido.")
             continue
-        attempts += 1
+        attempts[1] += 1
         comparison_result = check_equality(user_guess, secret_number)
         if comparison_result == 0:
             print("¡Felicidades! Has adivinado el número.")
@@ -27,12 +28,13 @@ def loop_until_correct(secret_number: int):
             print("Intenta con un número más alto.")
     return attempts
 
-def start_game():
+def start_game() -> None:
     secret_number = random.randint(1, 20)
     print("¡Bienvenido al juego de adivinar el número!")
     print("¿Puedes adivinar el número secreto entre 1 y 20?")
     attempts = loop_until_correct(secret_number)
-    print("Número de intentos:", attempts)
+    print("Número de intentos realizados:", attempts[0])
+    print("Número de intentos acertados:", attempts[1])
     print("Gracias por jugar. ¡Hasta la próxima!")
     
 if __name__ == "__main__":
